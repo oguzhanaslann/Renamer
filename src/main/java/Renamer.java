7f8b1d57633a81a019c5a8d6420f3164d6b2a3b6
oguzhanaslann <oguz21199@gmail.com>
2022-12-14 19:58:25 +0300

    - updates

--- src/main/java/Renamer.java
+++ src/main/java/Renamer.java
@@ -1,9 +1,11 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 
 public class Renamer {
-    public static final String PATH_OF_DIR = "/Users/oguzhanaslan/AndroidStudioProjects/sample_avatars";
-    public static final String FILE_MIME_TYPE = ".jpeg";
-    public static final String RENAME_TEMPLATE = "ic_avatar_<x>"+ FILE_MIME_TYPE ;
+    public static final String PATH_OF_DIR = "/.../.../.../...";
+    public static final String FILE_MIME_TYPE = ".png";
+    public static final String RENAME_TEMPLATE = "ic_alianz_<x>" + FILE_MIME_TYPE;
 
     public static void main(String[] args) {
         File file = new File(PATH_OF_DIR);
@@ -14,9 +16,14 @@ public class Renamer {
         System.out.println(files.length);
         for (int childIndex = 0; childIndex < files.length; childIndex++) {
             File child = files[childIndex];
-            String fileNewName = RENAME_TEMPLATE.replace("<x>",Integer.toString(childIndex));
-            File newNamedFile = new File(PATH_OF_DIR+"/"+ fileNewName);
+            String fileNewName = RENAME_TEMPLATE.replace("<x>", suffixOfFile(childIndex));
+            File newNamedFile = new File(PATH_OF_DIR + "/" + fileNewName);
             child.renameTo(newNamedFile);
         }
+    }
+
+    @NotNull
+    private static CharSequence suffixOfFile(int childIndex) {
+        return "x" + (childIndex + 1 );
     }
 }
